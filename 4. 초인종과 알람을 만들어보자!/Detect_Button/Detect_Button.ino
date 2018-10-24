44441daf73ce7a0c8d84392cb808e7533c55144f
user <user@LAPTOP-84MER62T>
2018-10-24 17:51:25 +0900

    Good

--- 4. 초인종과 알람을 만들어보자!/Detect_Button/Detect_Button.ino
+++ 4. 초인종과 알람을 만들어보자!/Detect_Button/Detect_Button.ino
@@ -1,18 +1,26 @@
 unsigned int P_led = 8;
 unsigned int P_bulb = 9;
+unsigned int P_buzzer = 10;
 unsigned int P_senser_light = A0;
 unsigned int P_senser_bio = 2;
+unsigned int P_senser_button = 3;
 
 bool C_led_auto = true;
 
 int D_sense_light;
 int D_sense_bio;
+int D_sense_button;
+
+int V_buzzer_lifetime = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode(P_led, OUTPUT);
+  pinMode(P_bulb, OUTPUT);
+  pinMode(P_buzzer, OUTPUT);
   pinMode(P_senser_light, INPUT);
   pinMode(P_senser_bio, INPUT);
+  pinMode(P_senser_button, INPUT);
   pinMode(P_bulb, INPUT);
   digitalWrite(P_led, HIGH);
 }
@@ -56,7 +64,32 @@ void loop() {
     }
   
     D_sense_light =  analogRead(P_senser_light);
-    D_sense_bio = digitalRead(P_senser_bio);
+    D_sense_bio = digitalRead(P_senser_bio);    
+    D_sense_button = digitalRead(P_senser_button);
+
+    if (D_sense_button == HIGH)
+    {
+      V_buzzer_lifetime = 600;
+    }
+    
+    if (V_buzzer_lifetime > 0)
+    {
+      if (V_buzzer_lifetime > 300)
+      {
+        tone(P_buzzer, 210);
+      }
+      else
+      {
+        tone(P_buzzer, 410);
+      }
+      --V_buzzer_lifetime;
+      delay(1);
+    }
+    else
+    {
+      noTone(P_buzzer);
+    }
+
     
 
     if (C_led_auto)
